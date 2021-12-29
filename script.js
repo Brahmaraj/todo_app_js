678c7345776ba25e8354d0e868f8f35b93d907ed
Brahmaraj <brahmarajrathod@gmail.com>
2021-12-29 09:15:57 +0530

    done mark button

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var cloned_list_item;
 var value_id;
 var done_button;
 var title_for_list;
+var temp;
 function modal(){
     document.getElementById("modal-div").style.display = "block";
 };
@@ -39,6 +40,15 @@ function addList(){
     cloned_list_item.style.color = 'black';
     cloned_list_item.setAttribute('id',`${Date.now()}`);
     cloned_list_item.setAttribute('value',`${Date.now()}`);
+    done_button = document.createElement('button');
+    done_button.setAttribute('id',`check-done-${Date.now()}`);
+    done_button.setAttribute('value',`${Date.now()}`);
+    // done_button.setAttribute('style','backgrond-color')
+    done_button.innerText = ' mark as done';
+    done_button.style.backgroundColor = 'grey';
+    console.log(done_button);
+    cloned_list_item.appendChild(done_button);
+    console.log(cloned_list_item);
     cloned_list_item.setAttribute('onClick',"completedTask(this.value)");
     console.log(document.getElementById(`${value_id}`));
     
@@ -54,10 +64,11 @@ function addList(){
     }
     //console.log(title_for_list);
     //document.getElementById(`${value_id}`).getElementsByTagName('hr').appendChild(cloned_list_item);
+    console.log(document.getElementById('check-done'));
     console.log(done_button);
     console.log(cloned_list_item);
     console.log(arr_of_obj);
-    document.getElementById(`${value_id}`).getElementsByClassName('add-list-after-this')[0].appendChild(cloned_list_item);
+    document.getElementById(`${value_id}`).getElementsByClassName('add-list-after-this')[0].appendChild(cloned_list_item).appendChild(done_button);
     //document.getElementById(`${value_id}`).insertBefore(cloned_list_item, )
     cloned_list_item = '';
 }
@@ -89,7 +100,7 @@ function deleteCard(val){
 
 function createCard(){
     if(arr_of_obj.size==0){
-    console.log("Empty");
+    document.getElementById('outer-container').innerHTML = "EMPTY";
     first_card = 0;
     }
     else {
@@ -97,19 +108,26 @@ function createCard(){
     display(first_card);
 }};
 function completedTask(value){
+    temp = document.getElementById(`${value}`).innerText;
+    document.getElementById(`${value}`).style.textDecoration = 'line-through';
     document.getElementById(`${value}`).style.color = 'red';
+    document.getElementById(`check-done-${value}`).innerHTML = '';
+    // document.getElementById(`${value}`).innerHTML = `<del>${temp}</del>`;
+    console.log(document.getElementById(`${value}`))
     //document.getElementById(`${abc}`).innerHTML =
     console.log(value);
 }
 function display(card){
     if(card==0){
-        console.log("Empty")
+        document.getElementById('outer-container').innerHTML = "EMPTY";
     }
     else {
     arr_of_obj.forEach(element => {
         card.id = element.id;
         card.querySelector(".card-head").innerHTML = element.title;
         card.setAttribute("value",`${element.id}`);
+        card.setAttribute("display","block");
+        card.setAttribute("min-height","300px");
         card.querySelector(".delete-button-in-card").setAttribute("value",`${element.id}`);
         card.querySelector(".delete-button-in-card").setAttribute("onClick","deleteCard(this.value)");
         card.querySelector(".add-button-in-card").setAttribute("value",`${element.id}`);
